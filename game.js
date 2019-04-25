cdc9f6d321e4dea2d9b5c9739ad9eb5e312cb51f
TheEBBGaming <theebbgaming@gmail.com>
2019-04-25 18:29:53 -0400

    Update game.js

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ function preload() {
   this.load.image('bug2', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_2.png');
   this.load.image('bug3', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/bug_3.png');
   this.load.image('platform', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/platform.png');
-  this.load.image('codey', 'http://pixelartmaker.com/art/ff9cc844923ac31.png');
+  this.load.image('codey', 'https://s3.amazonaws.com/codecademy-content/courses/learn-phaser/physics/codey.png');
 }
 
 const gameState = {
